e4f14bd2629cd049d4a132f35ed125844d2e784d
Vladymir Bezerra <vlad.bezerra@gmail.com>
2024-04-17 13:27:37 -0300

    Remove unused code

--- src/core.clj
+++ src/core.clj
@@ -9,5 +9,4 @@
   (when (= 1 (count args))
     (when-let [config (read-string (first args))]
       (println config)
-      #_(s/serve-persistent (:port config) #(s/http-handler % (:base-dir config)))
       (s/the-server (:port config) #(s/http-handler % (:base-dir config))))))
